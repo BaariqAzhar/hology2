85199508c6cadf1b68449053f32af7a29a9d7f15
ripstudent <ripstudent@gmail.com>
2018-07-16 00:13:40 +0700

    Nambah View Dari adel

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,47 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/test', function () {
-    return view('beranda/home');
-});
-
 
 Route::get('/', function () {
-    return view('lomba/ctf');
+    return view('beranda/home');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/appinov', function(){
+    return view('app_inovation');
+});
+
+Route::get('/news', function(){
+    return view('artikel_berita');
+});
+
+Route::get('/bizzit', function(){
+    return view('business_it_case');
+});
+
+Route::get('/bplan', function(){
+    return view('business plan');
+});
+
+Route::get('/ctf', function(){
+    return view('capture the flag');
+});
+
+Route::get('faq', function(){
+    return view('faq');
+});
+
+Route::get('games', function(){
+    return view('games');
+});
+
+Route::get('cp', function(){
+    return view('programming');
+});
+
+Route::get('smartdev', function(){
+    return view('smart device');
+});
